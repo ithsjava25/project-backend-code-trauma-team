bd23ee639e7202427bb558256e9ee8b106dca233
Linus Westling <westling.linus@gmail.com>
2026-03-25 07:54:12 +0100

    Initial commit

---
+++ src/test/java/org/example/projektarendehantering/ProjektArendehanteringApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.projektarendehantering;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjektArendehanteringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
