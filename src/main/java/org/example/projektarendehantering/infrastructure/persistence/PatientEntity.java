bae39d6b1833f953f57619734ac3c57ff5353df1
Linus Westling <westling.linus@gmail.com>
2026-03-31 14:52:52 +0200

    fixed imports again

--- src/main/java/org/example/projektarendehantering/infrastructure/persistence/PatientEntity.java
+++ src/main/java/org/example/projektarendehantering/infrastructure/persistence/PatientEntity.java
@@ -1,10 +1,6 @@
 package org.example.projektarendehantering.infrastructure.persistence;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
-import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.*;
 
 import java.time.Instant;
 import java.util.UUID;
