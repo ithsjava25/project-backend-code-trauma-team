b19a1e227e906b2ab31f336d86012ab6c7269fc3
Oscar Nidemar <oscar.nidemar@gmail.com>
2026-04-10 13:46:30 +0200

    chore: Upgrade Spring Cloud AWS to 4.0.0 for Spring Boot 4.0.4 compatibility
    - Use Spring Cloud AWS BOM 4.0.0
    - Revert manual S3Config workaround
    - Remove autoconfiguration exclusions in ProjektArendehanteringApplication
    - Update tests to verify S3 upload with ObjectMetadata

--- src/main/java/org/example/projektarendehantering/ProjektArendehanteringApplication.java
+++ src/main/java/org/example/projektarendehantering/ProjektArendehanteringApplication.java
@@ -3,12 +3,7 @@ package org.example.projektarendehantering;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication(exclude = {
-        io.awspring.cloud.autoconfigure.s3.S3AutoConfiguration.class,
-        io.awspring.cloud.autoconfigure.core.CredentialsProviderAutoConfiguration.class,
-        io.awspring.cloud.autoconfigure.core.RegionProviderAutoConfiguration.class,
-        io.awspring.cloud.autoconfigure.core.AwsAutoConfiguration.class
-})
+@SpringBootApplication
 public class ProjektArendehanteringApplication {
 
     public static void main(String[] args) {
